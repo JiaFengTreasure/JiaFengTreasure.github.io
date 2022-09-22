57461368bfa5bdd55be8bf8d12fc4f7b3bf16068
JiaFengTreasure <114091560+JiaFengTreasure@users.noreply.github.com>
2022-09-22 15:29:18 +0800

    Update app.cc709a3bd12490de0fd5.js

--- static/js/app.cc709a3bd12490de0fd5.js
+++ static/js/app.cc709a3bd12490de0fd5.js
@@ -164,7 +164,7 @@ html{
   perspective: 1000px;
 }
 .styleEditor {
-  position: fixed; left: 0; top: 0;
+  position: fixed; left: 0; top: 10x;
   -webkit-transition: none;
   transition: none;
   -webkit-transform: rotateY(10deg) translateZ(-100px) ;
@@ -405,7 +405,7 @@ html{
   perspective: 1000px;
 }
 .styleEditor {
-  position: fixed; left: 0; top: 0;
+  position: fixed; left: 0; top: 10px;
   transform: rotateX(-10deg) translateZ(-50px) ;
 }
 
